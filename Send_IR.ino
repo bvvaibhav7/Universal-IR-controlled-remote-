2b5ab4ce53a9e64f3af453db71c455d9a8972d06
bvvaibhav7 <47553156+bvvaibhav7@users.noreply.github.com>
2019-10-13 20:36:39 +0530

    Update Send_IR.ino

--- Send_IR.ino
+++ Send_IR.ino
@@ -1,9 +1,10 @@
+//Created by Venkata Vaibhav Bhagavatula on 9/10/2019
 #include <IRLibAll.h>
 IRsend mySender;
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);                   //baud rate
 }
 void loop() {
-   mySender.send(#protocol_no, #hex code, #no_of_bits);
+   mySender.send(#protocol_no, #hex code, #no_of_bits); //Send the IR signal
   delay(1000);
 }
